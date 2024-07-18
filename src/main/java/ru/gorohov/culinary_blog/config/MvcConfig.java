a97b8c352713ab07b3b64f350fc7120dc0a04692
zzzzDenzzzz <12den24@gmail.com>
2024-07-18 13:28:40 +0300

    add messages.properties

--- src/main/java/ru/gorohov/culinary_blog/config/MvcConfig.java
+++ src/main/java/ru/gorohov/culinary_blog/config/MvcConfig.java
@@ -1,5 +1,8 @@
 package ru.gorohov.culinary_blog.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import ru.gorohov.culinary_blog.interceptor.UserInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,5 +13,13 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new UserInterceptor());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
--- src/main/java/ru/gorohov/culinary_blog/controllers/AuthorizationController.java
+++ src/main/java/ru/gorohov/culinary_blog/controllers/AuthorizationController.java
@@ -1,6 +1,8 @@
 package ru.gorohov.culinary_blog.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import ru.gorohov.culinary_blog.dto.RegistrationDTO;
 import ru.gorohov.culinary_blog.models.User;
 import ru.gorohov.culinary_blog.services.UserService;
@@ -24,10 +26,14 @@ import jakarta.validation.Valid;
 @RequiredArgsConstructor
 public class AuthorizationController {
     private final UserService userService;
+    private final MessageSource messageSource;
 
     @GetMapping("/login")
     public String getLogin(@RequestParam(required = false) Boolean error, Model model) {
-        if (error != null && error) model.addAttribute("loginError", "Wrong login or password!");
+        if (error != null && error) {
+            model.addAttribute("loginError", messageSource
+                    .getMessage("login.error", null, LocaleContextHolder.getLocale()));
+        }
         return "authorization/login";
     }
 
@@ -51,37 +57,44 @@ public class AuthorizationController {
     private boolean validateRegisterRequest(BindingResult result, RegistrationDTO dto) {
         boolean error = false;
         if (dto.getPassword() == null || dto.getPassword().isBlank()) {
-            result.rejectValue("password", "error.password", "Password field is required!");
+            result.rejectValue("password", "error.password", messageSource
+                    .getMessage("registration.password.required", null, LocaleContextHolder.getLocale()));
             error = true;
         }
 
         if (dto.getRepeatPassword() == null || dto.getRepeatPassword().isBlank()) {
-            result.rejectValue("repeatPassword", "error.repeatPassword", "Repeat password field is required!");
+            result.rejectValue("repeatPassword", "error.repeatPassword", messageSource
+                    .getMessage("registration.repeatPassword.required", null, LocaleContextHolder.getLocale()));
             error = true;
         }
 
         if (dto.getEmail() == null || dto.getEmail().isBlank()) {
-            result.rejectValue("email", "error.email", "Email field is required!");
+            result.rejectValue("email", "error.email", messageSource
+                    .getMessage("registration.email.required", null, LocaleContextHolder.getLocale()));
             error = true;
         }
 
         if (dto.getUsername() == null || dto.getUsername().isBlank()) {
-            result.rejectValue("username", "error.username", "Username field is required!");
+            result.rejectValue("username", "error.username", messageSource
+                    .getMessage("registration.username.required", null, LocaleContextHolder.getLocale()));
             error = true;
         }
 
         if (!dto.getPassword().equals(dto.getRepeatPassword())) {
-            result.rejectValue("password", "error.passwordMismatch", "Passwords must match!");
+            result.rejectValue("password", "error.passwordMismatch", messageSource
+                    .getMessage("registration.password.mismatch", null, LocaleContextHolder.getLocale()));
             error = true;
         }
 
         if (userService.isEmailExists(dto.getEmail())) {
-            result.rejectValue("email", "error.emailExists", "There is already a user with this email!");
+            result.rejectValue("email", "error.emailExists", messageSource
+                    .getMessage("registration.email.exists", null, LocaleContextHolder.getLocale()));
             error = true;
         }
 
         if (userService.isUsernameExists(dto.getUsername())) {
-            result.rejectValue("username", "error.usernameExists", "There is already a user with this username!");
+            result.rejectValue("username", "error.usernameExists", messageSource
+                    .getMessage("registration.username.exists", null, LocaleContextHolder.getLocale()));
             error = true;
         }
 
