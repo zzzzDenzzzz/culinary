bde15ec65e1589bb8050ef01544aa8b0a79b7829
zzzzDenzzzz <12den24@gmail.com>
2024-07-18 07:27:06 +0300

    add error

---
+++ src/main/java/ru/gorohov/culinary_blog/controllers/CustomErrorController.java
@@ -0,0 +1,20 @@
+package ru.gorohov.culinary_blog.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("jakarta.servlet.error.status_code");
+        if (statusCode != null && statusCode == 404) {
+            return "redirect:/";
+        }
+
+        return "error";
+    }
+}
